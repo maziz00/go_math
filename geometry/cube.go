93fe5a85ebf93c950f3a7d0f6b2d57e74cbfa1d2
maziz00 <moh.azizux@gmail.com>
2021-05-22 15:03:48 +0400

    error value

--- geometry/cube.go
+++ geometry/cube.go
@@ -6,6 +6,6 @@ func CubeVolume(n int) (int, error) {
 	if n != 0{
 		return n * n * n 
 	} else {
-		return 0, error.New("Zero lenth edge is not allowed")
+		return 0, errors.New("Zero lenth edge is not allowed")
 	} 
 }
